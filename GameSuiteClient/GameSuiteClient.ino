0e2742c0a60fe9f6ac7a8bab6c266a192c5be97d
Angel <79809585+AngelOfTheSkyGod@users.noreply.github.com>
2024-04-06 14:27:28 -0500

    create screen stuff

--- GameSuiteClient/GameSuiteClient.ino
+++ GameSuiteClient/GameSuiteClient.ino
@@ -1,8 +1,10 @@
 #include <EEPROM.h>
+#include <UTFT.h>
 
 // ESP to client codes
 #define NETWORK_ACK 0
 #define SERVER_RESPONSE 1
+
 
 // Client to ESP codes
 #define SET_NETWORK 0
@@ -11,21 +13,16 @@
 #define debugSerial Serial
 #define espSerial Serial1
 
-
-//tft code
-#include <TFT.h>  // Hardware-specific library
-#include <SPI.h>
-
-#define CS 10
-#define DC 9
-#define RESET 8
+//Server Client commands
+#define PONG 0
 
 // pin definition for the Leonardo
 // #define CS   7
 // #define DC   0
 // #define RESET  1
 
-TFT myScreen = TFT(CS, DC, RESET);
+UTFT myScreen(CTE40, 38, 39, 40, 41);
+extern uint8_t BigFont[];
 
 // initial position of the point is the middle of the screen
 // initial position of the point is the middle of the screen
@@ -49,28 +46,43 @@ long millisOnStart;
 bool connected;
 bool verified;
 
+
+void printDebug(char * message){
+    debugSerial.println(message);
+    myScreen.fillScr(255, 0 ,0);
+    myScreen.setColor(0, 0, 0);
+    myScreen.print(message, 240, 0);
+
+    int yCoord = 0;
+    int length = strlen(message);
+
+    while (length > 30) {
+        char submessage[31];
+        memcpy(submessage, message, 30);
+        submessage[30] = '\0';
+
+        myScreen.print(submessage, 0, yCoord);
+        length -= 30;
+        yCoord += 16;
+        message += 30;
+    }
+
+    myScreen.print(message, 0, yCoord);
+}
+//320Y
+//480X
 void setup() {
   debugSerial.begin(9600);
   espSerial.begin(115200);
   connected = false;
   verified = false;
-  debugSerial.println(F("Started! Waiting for init ack from ESP..."));
+  myScreen.InitLCD();
+  myScreen.setBackColor(255, 255, 255);
+  myScreen.setFont(BigFont);
+  printDebug("Started! Waiting for init ack from ESP...");
 
   int count = 0;
-  myScreen.begin();
 
-  myScreen.background(0, 0, 0);  // clear the screen
-    myScreen.begin();
-
-  myScreen.background(0, 0, 0);
-
-  myScreen.stroke(255, 255, 255);
-
-  myScreen.setTextSize(2);
-
-  myScreen.text("Sensor Value : ", 0, 0);
-
-  myScreen.setTextSize(5);
   do {
     while (!espSerial.available()) {
       delay(50);
@@ -101,9 +113,7 @@ void loop() {
 
   if (espSerial.available() >= 2) {
     byte command = espSerial.read();
-    debugSerial.print(F("Cmd "));
-    debugSerial.println(command);
-
+    printDebug("Cmd " + command);
     byte length = espSerial.read();
 
     while (espSerial.available() < length) {
@@ -118,14 +128,20 @@ void loop() {
 }
 
 void setNetwork(char* ssid, char* pass) {
-    debugSerial.print(F("Sending SET_NETWORK to connect to SSID '"));
-    debugSerial.print(ssid);
-    debugSerial.println("'...");
+    printDebug("Sending SET_NETWORK to connect to SSID '");
+    printDebug(ssid);
+    printDebug("'...");
 
     espSerial.write(SET_NETWORK);
     espSerial.write(SET_NETWORK_LEN);
     espSerial.write(ssid, 32);
     espSerial.write(pass, 64);
+}
+
+void processServerCommand(byte command, byte length, char * data){
+  if (command == PONG){
+    printDebug("PONG");
+  }
 }
 
 void processESP(byte command, byte length, char* data) {
@@ -134,15 +150,16 @@ void processESP(byte command, byte length, char* data) {
       bool success = data[0];
 
       if (!success) {
-        debugSerial.println(F("Network connection failed! Trying again..."));
+        printDebug("Network connection failed! Trying again...");
         connected = false;
         verified = false;
       } else {
-        debugSerial.println(F("Network connection successful!"));
+        printDebug("Network connection successful!");
         verified = true;
       }
       break;
     case SERVER_RESPONSE:
-      bool command = data[0];
+      byte serverCommand = data[0];
+      processServerCommand(serverCommand, length, data);
   }
 }
